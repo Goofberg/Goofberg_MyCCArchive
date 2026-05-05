5da226d1b8227f00062c7e9a21681af5d737bdac
Goofberg <146931234+Goofberg@users.noreply.github.com>
2026-05-05 19:25:40 +0800

    Update installer.lua

--- SecurityTriggered/installer.lua
+++ SecurityTriggered/installer.lua
@@ -1,4 +1,4 @@
-local base = "https://raw.githubusercontent.com/Goofberg/McCCTweaked-SporeReconTurtle/main/"
+local base = "https://raw.githubusercontent.com/Goofberg/McCCTweaked-SporeReconTurtle/main/SecurityTriggered"
 
 local function download(url, path)
   local res = http.get(url)
