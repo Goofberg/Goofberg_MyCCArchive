f35bb3640b938f15ace9626f4313fd93054f3558
Goofberg <146931234+Goofberg@users.noreply.github.com>
2026-07-20 22:25:04 +0800

    Add configuration file for PlayerDetection

---
+++ PlayerDetection/config.lua
@@ -0,0 +1,8 @@
+return {
+    WEBHOOK_URL = "https://discord.com/api/webhooks/XXXX/YYYY", -- for the embed
+    BOT_TOKEN   = "YOUR_BOT_TOKEN_HERE",                          -- for DMs
+    DM_USER_ID  = "609378018510635010",
+    IGNORE_NAME = "goofberg",
+    HALF_SIZE   = 40, -- 80x80x80 box = ±40 on each axis from origin
+    CHECK_EVERY = 5,  -- seconds between scans
+}
